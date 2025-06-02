172dd37e58c565a5c0180b756f3efd1d343acd71
Stephen430 <149912385+Stephen430@users.noreply.github.com>
2025-06-02 08:22:18 +0100

    commit

--- obj/Debug/net9.0/StudentCounsellingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentCounsellingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentCounsellingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944530eb72ee5870c97474f1f85fcee18ac188c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c3e43e0ccf5b8f8b5406e97ca173e84cbc91ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentCounsellingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentCounsellingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
